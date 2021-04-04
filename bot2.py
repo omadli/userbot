402cb86efb6eac61eaf8f78a481bd7366c5e425b
omadli <76625693+omadli@users.noreply.github.com>
2021-04-04 21:08:17 +0500

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -10,7 +10,7 @@ import random
  
 app = Client("mr_pythoncik", 2625259, "0ddd178b7a1e5d3b220f9905b5d8f85a")
 
-get = channels.GetMessages(channel="@O_romitan", id=[3])
+get = app.get_messages(channel="@O_romitan", id=[3])
 msgs = get.messages
 rm = msgs[0].reply_markup
 print(rm)
